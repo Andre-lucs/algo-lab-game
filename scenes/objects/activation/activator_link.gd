6c209ef8710da5addfa6e612afbd0fda3d2f99d9
Andre-lucs <andreltd312@gmail.com>
2025-05-21 20:09:35 -0300

    Refactor ActivatorLink and Link classes: replace Path2D with Link, update connection handling, and enhance point management for better activation functionality.

--- scenes/objects/activation/activator_link.gd
+++ scenes/objects/activation/activator_link.gd
@@ -1,41 +1,32 @@
-extends Path2D
+extends Link
 class_name ActivatorLink
 
 @export var origin : Activatable
 @export var destination : Activatable
-@export var origin_point : Vector2
-@export var destination_point : Vector2
-
-@onready var line : ClickableLine2D = $ClickableLine2D
-@onready var menu : ObjectPopupMenu = $ObjectPopupMenu
-@onready var plug : Node2D = $Plug
 
 signal activated_destination(destination:Activatable)
 
 func _ready() -> void:
-	if origin == null or destination == null:
-		print("Origin or destination not set")
+	super()
+	assert(origin_connection != null, "Origin connection is null")
+	assert(destination_connection != null, "Destination connection is null")
+	if origin_connection.activatable == null or destination_connection.activatable == null:
+		printerr("Origin or destination_node not set")
+		delete()
 		return
-	
-	line.add_point(origin_point)
-	line.add_point(destination_point)
-	plug.position = destination_point
-	plug.rotation = (destination_point - origin_point).angle()
+
+	origin = origin_connection.activatable
+	destination = destination_connection.activatable
 
 	# Connect the trigger_signals
 	var triggers = origin.get_trigger_signals()
 
 	for trigger in triggers:
-		origin.target.connect(trigger, activate)
+		origin.target.connect(trigger, func(_arg0 = null, _arg1 = null,_arg2 = null,_arg3 = null,_arg4 = null): activate())
 	
-
-func activate(_arg0 = null, _arg1 = null,_arg2 = null,_arg3 = null,_arg4 = null) -> void:
+func activate() -> void:
 	destination.activate()
 	activated_destination.emit(destination)
-
-func _on_clickable_line_2d_clicked(_event : InputEventMouseButton, _global_position:Vector2, _segment:int, _offset:float) -> void:
-	if _event.is_action_pressed("object_menu"):
-		menu.show_popup()
 
 func _on_object_popup_menu_clicked_option(idx:int) -> void:
 	match idx:
@@ -46,10 +37,3 @@ func _on_object_popup_menu_clicked_option(idx:int) -> void:
 
 func delete() -> void:
 	queue_free()
-
-func _exit_tree() -> void:
-	# Disconnect the trigger_signals
-	var triggers = origin.get_trigger_signals()
-
-	for trigger in triggers:
-		origin.target.disconnect(trigger, activate)
--- scenes/utils/link/link.gd
+++ scenes/utils/link/link.gd
@@ -2,6 +2,8 @@ class_name Link extends EditableLine2D
 
 @export var origin_connection : ConnectionArea
 @export var destination_connection : ConnectionArea
+@export var origin_point : Vector2
+@export var destination_point : Vector2
 
 @onready var path : Path2D = $Path2D
 @onready var menu : ObjectPopupMenu = $ObjectPopupMenu
@@ -10,14 +12,22 @@ class_name Link extends EditableLine2D
 
 signal updated_path
 
+# TODO: Make Path optional (ex: activation links don't need a path)
+# TODO: Make link listen to start and end to check if they still overlap a connection area if not give a future warning AND reconect the link moving the point to the connection area
+
 func _ready() -> void:
 		super()
 		if origin_connection == null or destination_connection == null:
 			printerr("Origin or destination_node not set")
 			return
+		
+		if not origin_point:
+			origin_point = to_local(origin_connection.global_position)
+		if not destination_point:
+			destination_point = to_local(destination_connection.global_position)
 
-		add_point(to_local(origin_connection.global_position))
-		add_point((destination_connection.global_position))
+		add_point(origin_point)
+		add_point(destination_point)
 
 		clicked.connect(_on_line_2d_clicked)
 		edited_line.connect(update_path.unbind(1))
@@ -25,8 +35,7 @@ func _ready() -> void:
 		update_path()
 
 func update_path():
-	if path.curve == null:
-		path.curve = Curve2D.new()
+	path.curve = Curve2D.new()
 	_update_start()
 	_update_end()
 	_sync_points()
@@ -34,7 +43,7 @@ func update_path():
 
 func _update_start():
 	if points.size() == 0 or origin_connection == null: return
-	points[0] = to_local(origin_connection.global_position)
+	points[0] = origin_point
 
 	var newPos = points[0]
 	start.position = newPos
@@ -44,8 +53,8 @@ func _update_end():
 	if points.size() == 0 or destination_connection == null: return
 	
 	if points.size() == 1:
-		add_point(to_local(destination_connection.global_position))
-	else: points[points.size()-1] = to_local(destination_connection.global_position)
+		add_point(destination_point)
+	else: points[points.size()-1] = destination_point
 
 	var newPos = points[points.size()-1]
 	end.position = newPos
