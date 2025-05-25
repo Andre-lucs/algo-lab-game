90b4ac523d72d1516803e871fd39ac2a245db2d9
Andre-lucs <andreltd312@gmail.com>
2025-05-25 15:31:34 -0300

    Add Router scene and script: implement number queue and movement logic

---
+++ scenes/objects/router/router.gd
@@ -0,0 +1,62 @@
+class_name Router extends Node2D
+
+@onready var rotative: Node2D = %Rotative
+@onready var numbers_container: Node2D = %Numbers
+@onready var number_movement: NumberMovement = %NumberMovement
+@onready var activatable: Activatable = $Traits/Activation
+
+var number_queue: Array[Number] = []
+## next_path index on number_movement.output_paths
+var next_path: int = 0
+
+func enqueue_number(number:Number) -> void:
+	number.move_to(Vector2.ZERO, numbers_container)
+	number_queue.append(number)
+
+	if number_queue.front() != number:
+		number.hide()
+
+func dequeue_number():
+	if number_queue.is_empty():
+		return
+	var path := _get_path()
+	if path == null:
+		printerr("No path available to dequeue number.")
+		return
+	var number := number_queue.pop_front() as Number
+	path.move_number(number)
+
+	if not number_queue.is_empty() : _number_scale_animation(number_queue.front())
+	_update_arrow(1)
+
+func _update_arrow(increment: int = 0) -> void:
+	if increment != 0:
+		next_path += increment
+	var path := _get_path()
+	if path == null:
+		return
+	create_tween().tween_property(rotative, "rotation", global_position.angle_to_point(path.to_global(path.points[1])), 0.2)
+
+func _get_path() -> MovementLink:
+	var paths := number_movement.output_paths
+	if paths.is_empty():
+		return null
+	if next_path >= paths.size():
+		next_path = 0
+	return paths[next_path]
+
+func _number_scale_animation(number: Number) -> void:
+	number.scale = Vector2.ZERO
+	number.show()
+	var anim = create_tween()
+	anim.tween_property(number, "scale", Vector2.ONE, 0.2).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	anim.play()
+
+func _on_menu_clicked_option(idx:int) -> void:
+	match idx:
+		0:
+			activatable.auto = !activatable.auto
+		1:
+			queue_free()
+		_:
+			print("Unknown option clicked: ", idx)
