e82dd512b08e7e27477614130fba5255f8479256
Andre-lucs <andreltd312@gmail.com>
2025-06-03 19:14:26 -0300

    Add NumberListing UI component with hover functionality and dynamic display

---
+++ scenes/ui/number_listing/number_listing.gd
@@ -0,0 +1,55 @@
+class_name NumberListing
+extends Control
+
+const SEPARATION := 10
+
+@export var mouse_area : MouseInteractionArea2D
+@export var show_on_hover := true
+
+@onready var base_num_label: Label = %BaseNumLabel
+@onready var numbers_container: HBoxContainer = $HBoxContainer
+
+var _required_width : float
+var numbers : Array[float]
+
+func _ready() -> void:
+	if not show_on_hover: return
+	if not mouse_area:
+		push_error("Show on hover is true but no MouseInteractionArea is set")
+		return
+	mouse_area.mouse_entered.connect(_on_mouse_entered)
+	mouse_area.mouse_exited.connect(_on_mouse_exited)
+
+func display():
+	_required_width = 0
+	for c in numbers_container.get_children():
+		c.free()
+	if numbers.is_empty():
+		return
+	for n in numbers:
+		var label := base_num_label.duplicate() as Label
+		label.text = str(n).replace(".0", "").replace(",0", "")
+		_required_width += label.text.length() * base_num_label.label_settings.font_size + SEPARATION
+		numbers_container.add_child(label)
+		numbers_container.add_child(VSeparator.new())
+		label.show()
+	var last_sep = numbers_container.get_children().pop_back()
+	if last_sep: last_sep.queue_free()
+	show()
+
+func get_required_width() -> float:
+	return _required_width;
+
+func update_display():
+	if visible:
+		display()
+		if numbers.is_empty():
+			hide()
+
+func _on_mouse_entered() -> void:
+	await get_tree().create_timer(.4).timeout
+	if not mouse_area.mouse_over:return
+	display()
+
+func _on_mouse_exited() -> void:
+	hide()
