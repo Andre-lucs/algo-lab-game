27f5131d23d15a6c95418293f29a919f82a7e863
Andre-lucs <andreltd312@gmail.com>
2025-06-27 16:53:06 -0300

    Add initial level selection screen and back navigation functionality

--- scenes/ui/screens/start_menu.gd
+++ scenes/ui/screens/start_menu.gd
@@ -1,10 +1,9 @@
 extends Control
 
-@export var initial_level : LevelPropsResource
+@onready var level_select_screen : PackedScene = load("uid://ci8l4lwwvvbjw")
+
+func _on_play_button_pressed() -> void:
+	SceneManager.change_scene(level_select_screen)
 
 func _on_play_test_pressed() -> void:
 	LevelManager.load_test_level()
-
-
-func _on_play_button_pressed() -> void:
-	LevelManager.load_level(initial_level)
