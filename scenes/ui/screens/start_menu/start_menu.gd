4a54bd1f5ee9dc939c35a48b147dafa24a09bfb2
Andre-lucs <andreltd312@gmail.com>
2025-07-15 13:15:47 -0300

    Implement start menu functionality with play and exit options

--- scenes/ui/screens/start_menu/start_menu.gd
+++ scenes/ui/screens/start_menu/start_menu.gd
@@ -2,8 +2,18 @@ extends Control
 
 @onready var level_select_screen : PackedScene = load("uid://ci8l4lwwvvbjw")
 
+func _ready() -> void:
+	if OS.has_feature("editor"):
+		%TestButton.visible = true
+	else:
+		%TestButton.visible = false
+	%PlayButton.grab_focus.call_deferred()
+
 func _on_play_button_pressed() -> void:
 	SceneManager.change_scene(level_select_screen)
 
 func _on_play_test_pressed() -> void:
 	LevelManager.load_test_level()
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
